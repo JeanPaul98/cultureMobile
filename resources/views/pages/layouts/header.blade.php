8fe62bcf185a79cb186b1dfc343762b51430aecb
jean paul <pauladzonyo79@gmail.com>
2023-11-24 12:59:45 +0100

    commit translate

--- resources/views/pages/layouts/header.blade.php
+++ resources/views/pages/layouts/header.blade.php
@@ -21,18 +21,20 @@
 
         <a href="#" class="mx-2"><span class="bi-facebook"></span></a>
         <a href="#" class="mx-2"><span class="bi-instagram"></span></a>
-
+        
+        <!-- ======= Search Form ======= -->
         <a href="#" class="mx-2 js-search-open"><span class="bi-search"></span></a>
         <i class="bi bi-list mobile-nav-toggle"></i>
-
-        <!-- ======= Search Form ======= -->
         <div class="search-form-wrap js-search-form-wrap">
           <form action="search-result.html" class="search-form">
             <span class="icon bi-search"></span>
             <input type="text" placeholder="Search" class="form-control">
             <button class="btn js-search-close"><span class="bi-x"></span></button>
           </form>
-        </div><!-- End Search Form -->
+        </div>
+        <!-- End Search Form -->
+        <a href="#" class="mx-2"><span class="bi-translate"></span></a>
+
 
       </div>
 
